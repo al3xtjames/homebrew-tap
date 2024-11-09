44e7d1f35f5484cff74cbf3b32a804537fb2d9f8
Alex James <git@alextjam.es>
2024-11-09 17:24:34 -0600

    intel-power-gadget: 3.7.0
    Based on the last upstream revision: https://github.com/Homebrew/homebrew-cask/blob/d7a2966798fb128c93824ba668fa6281942b2e97/Casks/i/intel-power-gadget.rb

--- Casks/geekbench@5.rb
+++ Casks/geekbench@5.rb
@@ -1,7 +1,6 @@
 cask "geekbench@5" do
   version "5.5.1"
   sha256 "04b06cb642e51230a3dfd07ce2d3a4ea696cb349583737622749174dc8747313"
-  depends_on macos: ">= :high_sierra"
 
   url "https://cdn.geekbench.com/Geekbench-#{version}-Mac.zip"
   name "Geekbench"
@@ -9,6 +8,7 @@ cask "geekbench@5" do
   homepage "https://www.geekbench.com/"
 
   auto_updates true
+  depends_on macos: ">= :high_sierra"
 
   app "Geekbench #{version.major}.app"
 
