4f3bd9b1553d2a0c997d46265929caeee6852070
Alex James <git@alextjam.es>
2025-03-30 17:06:23 -0500

    mpv: 0.40.0

--- Casks/mpv.rb
+++ Casks/mpv.rb
@@ -1,15 +1,15 @@
 cask "mpv" do
-  version "0.39.0-1"
+  version "0.40.0"
 
   on_arm do
-    url "https://web.archive.org/web/20240926120208/https://nightly.link/mpv-player/mpv/actions/runs/10999492923/mpv-macos-14-arm.zip"
-    sha256 "c2724fc11b0db2b01016e08d180250d0b9398fbbe18a59cf75bbe88bad696b0e"
+    url "https://web.archive.org/web/20250330220503/https://nightly.link/mpv-player/mpv/actions/runs/14066361747/mpv-macos-14-arm.zip"
+    sha256 "b02dc997eb2c88c87b2f76489636e7c7eb90ae8754879106e76a5cd8129f24b9"
     depends_on macos: ">= :sonoma"
   end
 
   on_intel do
-    url "https://web.archive.org/web/20240926130345/https://nightly.link/mpv-player/mpv/actions/runs/10999492923/mpv-macos-13-intel.zip"
-    sha256 "f5235dfb13a7bee1b92e2875edc1263f0c37d23c95c353e86276a4f85fceb326"
+    url "https://web.archive.org/web/20250330220356/https://nightly.link/mpv-player/mpv/actions/runs/14066361747/mpv-macos-13-intel.zip"
+    sha256 "ad8bfa7df4591958cf7f35996e5fe3c59f5ac4b4cee5d94e36e2344b11cd4a5a"
     depends_on macos: ">= :ventura"
   end
 
