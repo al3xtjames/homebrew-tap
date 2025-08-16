2b7b79a33adb14343d5f07e80edd49e6f873f0b7
Alex James <git@alextjam.es>
2025-08-15 17:58:08 -0700

    musicbrainz-picard@nightly: 3.0.0.dev6+2096.c8cbe3186.20250815163724

--- Casks/musicbrainz-picard@nightly.rb
+++ Casks/musicbrainz-picard@nightly.rb
@@ -1,14 +1,14 @@
 cask "musicbrainz-picard@nightly" do
-  version "3.0.0.dev6+2070.8cd270f17.20250812081111"
+  version "3.0.0.dev6+2096.c8cbe3186.20250815163724"
 
   on_arm do
-    url "https://nightly.link/metabrainz/picard/actions/runs/16902854489/macos-app-11.0-arm64.zip"
-    sha256 "0e4df00da0b9f968bb5ac2b92a24def4d57032264177fb6c5f8e6e0bcc29bf4a"
+    url "https://nightly.link/metabrainz/picard/actions/runs/16994571495/macos-app-11.0-arm64.zip"
+    sha256 "908a40297daa33800d91e175f59a8657d688f46f25f498bc50860bef0cace113"
   end
 
   on_intel do
-    url "https://nightly.link/metabrainz/picard/actions/runs/16902854489/macos-app-11.0-x86_64.zip"
-    sha256 "3016aa8e37d997011303bb1bd4d6a72d228b68fd08f82e66afcc1d02241019d9"
+    url "https://nightly.link/metabrainz/picard/actions/runs/16994571495/macos-app-11.0-x86_64.zip"
+    sha256 "9a1e64c1126321f7e5bb3522914f01b7947d1c5fc4c00425565577adb7b81d49"
   end
 
   name "MusicBrainz Picard"
